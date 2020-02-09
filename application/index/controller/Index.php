bb642e18bd11b2015ba4dbb6282a29e278538512
rayworld <casp@sina.com>
2020-02-09 16:36:17 +0800

    开张了

---
+++ application/index/controller/Index.php
@@ -0,0 +1,44 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Index extends Controller
+{
+    /*
+     public function hello1($name = 'thinkphp')
+    {
+        //$data = Db::name('data')->find();
+        $data = '';
+        $this->assign('result', $data);
+        return $this->fetch();        
+    }
+
+    public function hello00($name = 'World')
+    {
+
+        // 获取当前URL地址 不含域名
+        echo 'url: ' . $this->request->url() . '<br/>';
+        return 'Hello,' . $name . '！';
+
+    }
+
+    public function hello(Request $request)
+    {
+        
+        echo 'name:'.$request->param('name','World','strtolower');
+        echo '<br/>test:'.$request->param('test','thinkphp','strtoupper');
+    } 
+    */
+    
+    //
+    public function index()
+    {
+        if(cookie('user_name') != '')
+        {
+            echo "您好： " . cookie('user_name') . ', <a href="' . url('login/loginout') . '">退出</a>';
+        }else
+        {
+            $this->redirect(url('index/login/index'));
+        }
+    } 
+}
