6a3b5419829fa3c11e7bc5e4dc5251fea8392cf4
rayworld <casp@sina.com>
2020-02-10 14:22:41 +0800

    半拉子项目基本完工

--- config/app.php
+++ config/app.php
@@ -142,5 +142,6 @@ return [
     'show_error_msg'         => true,
     // 异常处理handle类 留空使用 \think\exception\Handle
     'exception_handle'       => '',
-
+    //web资源根目录
+    'web_root'              => '/static/common',
 ];
