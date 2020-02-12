9c805702e17a6cb243c1fa796e8059deba540f90
rayworld <casp@sina.com>
2020-02-12 13:23:38 +0800

    think PHP5 api 测试完成

---
+++ application/api/controller/v1/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\api\controller\v1;
+
+class User
+{
+    public function read()
+    {
+        $data = ['sdfs','sdfsd'];
+        
+        return json($data);
+    }
+}
--- route/route.php
+++ route/route.php
@@ -22,7 +22,7 @@
 
 
 
-Route::get('test','api/test/index');
+Route::get(':version.user/read','api/:version.User/read');
 Route::post('test/:id','api/test/update');
 Route::delete('test/:id','api/test/delete');
 Route::put('test','api/test/save');
