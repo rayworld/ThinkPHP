c14bc8f78a00be4b4803df8dc04251a2276a3894
rayworld <casp@sina.com>
2020-02-13 20:34:37 +0800

    连接MS SQL Server 1012

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,47 +1,48 @@
 <?php
+
 namespace app\admin\controller;
- 
+
 use think\Controller;
 use think\Request;
 use think\Db;
- 
+
 class Login extends Controller
 {
     //xianshi denglu
     public function index()
     {
         $request = request();
-        if($request->isPost()){
+        if ($request->isPost()) {
             // 做一个简单的登录 组合where数组条件 
-            $map=input('post.');
+            $map = input('post.');
             // dump($map);
             // exit();
-            $map['password']=md5($map['password']);
-            $data=Db::name('users')->where($map)->find();
+            $map['password'] = md5($map['password']);
+            $data = Db::connect("mysql")->name('users')->where($map)->find();
             if (empty($data)) {
                 $this->error('账号或密码错误');
-            }else{
-                $sdata=[
-	                'id'=>$data['id'],
-	                'username'=>$data['username'],
-	                'avatar'=>$data['avatar'],
-	                'email'=>$data['email'],
-	                'phone'=>$data['phone'],
+            } else {
+                $sdata = [
+                    'id' => $data['id'],
+                    'username' => $data['username'],
+                    'avatar' => $data['avatar'],
+                    'email' => $data['email'],
+                    'phone' => $data['phone'],
                 ];
-                session('user',$sdata);
+                session('user', $sdata);
 
-                $this->success('登录成功、前往管理后台','admin/Index/index');
+                $this->success('登录成功、前往管理后台', 'admin/Index/index');
             }
-        }else{
-        return $this->fetch();
+        } else {
+            return $this->fetch();
         }
     }
-    
+
 
     // 退出登录
     public function loginOut()
     {
-        session('user',null);
-        $this->success('退出成功、前往登录页面','Index/login/index');
+        session('user', null);
+        $this->success('退出成功、前往登录页面', 'Index/login/index');
     }
 }
--- application/api/controller/v1/User.php
+++ application/api/controller/v1/User.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace app\api\controller\v1;
+
+use think\Db;
 
 class User
 {
     public function read()
     {
-        $data = ['sdfs','sdfsd'];        
+        //$data = ['sdfs','sdfsd'];
+        $data = Db::connect("mssql")->name('t_User_Bak')->select();
         return json($data);
     }
 }
